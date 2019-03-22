f1d836e0eac1cbf7f608d8fa2e7bc508ffeae1d2
Marc Bellêtre <marc@pixelparfait.fr>
2019-03-22 10:59:00 +0100

    Fix sass-loader and postcss-loader execution order

--- webpack.config.js
+++ webpack.config.js
@@ -45,7 +45,7 @@ module.exports = (env, argv) => {
         },
         {
             test: /\.s?css$/i,
-            use: ['style-loader', 'css-loader?sourceMap=true', 'sass-loader', 'postcss-loader']
+            use: ['style-loader', 'css-loader?sourceMap=true', 'postcss-loader', 'sass-loader']
         });
     }
 
@@ -57,7 +57,7 @@ module.exports = (env, argv) => {
         },
         {
             test: /\.s?css/i,
-            use : [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader', 'postcss-loader']
+            use : [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
         });
     }
 
