08b666efa675d0bf2af6189c2d64f6895cd39651
Marc Bellêtre <marc@pixelparfait.fr>
2019-05-29 11:48:56 +0200

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 var config = {
-    entry: ['./src/js/app.js', './src/sass/main.scss'],
+    entry: ['./src/js/app.js'],
     output: {
         filename: 'js/bundle.js',
         path: path.resolve(__dirname, 'public/assets')
@@ -11,20 +11,47 @@ var config = {
     module: {
         rules: [
             {
-                test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
+                test: /\.(eot|otf|svg|ttf|woff|woff2)(\?.*)?$/,
+                include: [
+                    path.resolve(__dirname, 'src/fonts')
+                ],
                 use: {
                     loader: 'file-loader',
                     options: {
-                        name: '[path][name].[ext]'
+                        outputPath: 'fonts',
+                        publicPath: '../fonts/',
+                        name: '[hash].[ext]'
                     }
                 }
             },
             {
+                test: /\.(png|jpe?g|gif|svg)$/,
+                exclude: [
+                    path.resolve(__dirname, 'src/fonts')
+                ],
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'img',
+                            name: '[name].[ext]'
+                        },
+                    },
+                ],
+            },
+            {
                 test: /\.(js)$/,
-                include: path.resolve(__dirname, '../src'),
+                exclude: /node_modules/,
                 loader: 'babel-loader'
+            },
+            {
+                test: /\.s?css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
             }
         ]
+    },
+    externals: {
+        jquery: 'jQuery'
     },
     plugins: [
         new webpack.LoaderOptionsPlugin({
@@ -33,28 +60,7 @@ var config = {
         new MiniCssExtractPlugin({
             filename: 'css/bundle.css'
         })
-    ],
-    externals: {
-        'jquery': 'jQuery'
-    }
+    ]
 };
 
-module.exports = (env, argv) => {
-    if (argv.mode === 'development') {
-        config.devtool = 'source-map';
-
-        config.module.rules.push({
-            test: /\.s?css$/i,
-            use: [MiniCssExtractPlugin.loader, 'css-loader?sourceMap=true', 'postcss-loader', 'sass-loader']
-        });
-    }
-
-    if (argv.mode === 'production') {
-        config.module.rules.push({
-            test: /\.s?css/i,
-            use : [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
-        });
-    }
-
-    return config;
-};
+module.exports = config;
